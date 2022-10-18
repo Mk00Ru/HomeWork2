1314469054514a24b16208683e8d82ef91bd305a
Makar <mvkasach0603@gmail.com>
2022-10-18 19:37:02 +0300

    Сделал 5 задание

---
+++ Task5.py
@@ -0,0 +1,16 @@
+# Реализуйте алгоритм перемешивания списка.
+
+import time
+
+
+def shuffle_list(origin_list):
+    shuffled_list = origin_list.copy()
+    len_list = len(origin_list)
+    for i in range(len_list):
+        rand_index = (int(time.time() + time.time_ns())) % len_list
+        shuffled_list[i], shuffled_list[rand_index] = shuffled_list[rand_index], shuffled_list[i]
+    return shuffled_list
+
+
+test_list = [1, "No", 3, 4, "Yes", "Nice", 7657, 24]  
+print(f"{test_list} --> {shuffle_list(test_list)}")
